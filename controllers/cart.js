eb65ea0b6e8eee20b664a50f3ea7712088299c79
Anshu200151 <rajbharnavin6@gmail.com>
2024-11-11 23:10:10 +0530

    Add to Cart Done

--- controllers/cart.js
+++ controllers/cart.js
@@ -1,6 +1,7 @@
 const Cart = require("../models/cart");
 const Product = require("../models/product");
 const { createCart, getAllCarts } = require("../services/cart");
+const logger = require("../wrapper/logger");
 
 const handleAddToCart = async (req, res) => {
   try {
@@ -22,11 +23,14 @@ const handleAddToCart = async (req, res) => {
     }
 
     const carts = await createCart(req, cart, price, quantity, productId);
-
-    res.status(200).json({ message: "Item added to cart", carts: carts });
+    logger.info("handleAddToCart :: Cart added Successfully ");
+    return res
+      .status(200)
+      .json({ message: "Item added to cart", carts: carts });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "server error", error });
+    logger.error("handleAddToCart :: error ", error);
+    return res.status(500).json({ message: "server error", error });
   }
 };
 
@@ -38,6 +42,7 @@ const handleGetAllCarts = async (req, res) => {
     return res.status(200).json({ carts: allCarts });
   } catch (error) {
     console.error(error);
+    logger.error("handleGetAllCarts :: error ", error);
     res.status(500).json({ message: "server error", error });
   }
 };
