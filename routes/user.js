d5be4de9c08047506e2c4e2118da22bcf3362e38
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-11-11 21:21:27 +0530

    validation check

--- routes/user.js
+++ routes/user.js
@@ -9,17 +9,16 @@ const { userAddValidationSchema, userUpdateValidationSchema, productValidationUp
 
 const userRouter = express.Router();
 
-userRouter.post("/add",userAddValidationSchema, authenticateToken(["SUPER_ADMIN"]), handleAddAdmin);
-userRouter.post("/add-partner",userAddValidationSchema,upload.single("profileImage"),authenticateToken(["ADMIN"]),handleAddPartner);
-userRouter.put("/verify-product/:updateId",productValidationUpdateSchema,upload.single("image"),authenticateToken(["ADMIN"]),handleUpdateProduct);
-userRouter.put("/update-user/:id",userUpdateValidationSchema,upload.single("image"),authenticateToken(["ADMIN"]),handleUpdateUserById);
+userRouter.post("/add", authenticateToken(["SUPER_ADMIN"]),userAddValidationSchema, handleAddAdmin);
+userRouter.post("/add-partner",authenticateToken(["ADMIN"]),upload.single("profileImage"),userAddValidationSchema,handleAddPartner);
+userRouter.put("/verify-product/:updateId",authenticateToken(["ADMIN"]),upload.single("image"),productValidationUpdateSchema,handleUpdateProduct);
+userRouter.put("/update-user/:id",authenticateToken(["ADMIN"]),upload.single("image"),userUpdateValidationSchema,handleUpdateUserById);
 userRouter.get("/partners-added-by-admin",authenticateToken(["ADMIN"]),handleGetAllPartnersAddedByAdmin);
 userRouter.get("/all-admins", authenticateToken(["SUPER_ADMIN"]), handleGetAllAdmin);
 userRouter.get("/all-partners",authenticateToken(["SUPER_ADMIN"]),handleGetAllPartner);
 userRouter.get("/all-products",authenticateToken(["SUPER_ADMIN"]),handleGetAllProducts);
 userRouter.get("/all-customers",authenticateToken(["SUPER_ADMIN"]),handleGetAllCustomer);
 userRouter.get("/verfyingproducts",authenticateToken(["ADMIN"]),handleGetProductsToVerifyByAdmin);
-
 
 
 
