be017d130f616ffa83e50d76c59c987702dce427
Anshu200151 <rajbharnavin6@gmail.com>
2024-11-14 11:59:46 +0530

    add order-details API

--- wrapper/imageUtils.js
+++ wrapper/imageUtils.js
@@ -2,32 +2,34 @@ const fs = require("fs");
 const path = require("path");
 const ImageModel = require("../models/image");
 
-async function getProductsWithImage(req,product) {
+async function getProductsWithImage(req, product) {
   try {
-    // Ensure the product has an image path
+    //Check product has an image path
     if (!product.image) {
       throw new Error("Product has no image path");
     }
 
-    // Construct the full path to the image
+    //full path to the image
     const imagePath = path.join(__dirname, "../public", product.image);
 
     // Read the image file asynchronously
     const imageBuffer = await fs.promises.readFile(imagePath);
 
     // Convert the image buffer to Base64
-    const base64Image = `data:image/jpeg;base64,${imageBuffer.toString("base64")}`;
+    const base64Image = `data:image/jpeg;base64,${imageBuffer.toString(
+      "base64"
+    )}`;
     const singleImage = await ImageModel.create({
-        name : req.file.filename,
-        image : base64Image
+      name: req.file.filename,
+      image: base64Image,
     });
     await singleImage.save();
 
     return singleImage;
-    
+
     // return {
     //   ...product.toObject(),
-    //   image: base64Image, 
+    //   image: base64Image,
     // };
   } catch (error) {
     console.error("Error converting image to Base64:", error.message);
