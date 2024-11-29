b1fe20b8e1ee7840c831b5a80c1ed1c643d35951
Saurabh Sawant <113126470+Saurabhsawant77@users.noreply.github.com>
2024-11-29 13:23:53 +0530

    storage issue resolved

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ const mainRouter = require("./routes");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(express.static(path.resolve("./public")));
 
 //auth route
